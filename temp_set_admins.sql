2d626209f20eb76c9785437be66fad7f42fe9115
Jordi Catala <jordicatalaamat@gmail.com>
2025-10-10 11:05:46 -0600

    chore: remove temporary admin SQL file

--- temp_set_admins.sql
+++
@@ -1,9 +0,0 @@
--- Set admin status for Jordi and Nina
-UPDATE profiles
-SET is_admin = true
-WHERE email IN ('jordicatalaamat@gmail.com', 'ninamorel.mv@gmail.com');
-
--- Verify
-SELECT id, email, name, is_admin
-FROM profiles
-WHERE email IN ('jordicatalaamat@gmail.com', 'ninamorel.mv@gmail.com');

fa3f7e6b5da6d3b4316d607458e713f3ef2d7bc9
Jordi Catala <jordicatalaamat@gmail.com>
2025-10-10 07:34:03 -0600

    chore: bump version to 1.2.1

--- apps/expo/app.config.js
+++ apps/expo/app.config.js
@@ -6,7 +6,7 @@ export default {
     slug: 'mazunte-connect',
     jsEngine: 'hermes',
     scheme: 'mazunteconnect',
-    version: '1.2.0',
+    version: '1.2.1',
     orientation: 'portrait',
     icon: './assets/icon.png',
     userInterfaceStyle: 'automatic',

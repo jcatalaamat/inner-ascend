2e41c44c94aa4bd2b5ab5cc4269de3942f2c2e13
Jordi Catala <jordicatalaamat@gmail.com>
2025-10-29 00:49:56 -0600

    chore: bump version to 1.0.1

--- apps/expo/app.config.js
+++ apps/expo/app.config.js
@@ -6,7 +6,7 @@ export default {
     slug: 'inner-ascend',
     jsEngine: 'hermes',
     scheme: 'innerascend',
-    version: '1.0.0',
+    version: '1.0.1',
     orientation: 'portrait',
     icon: './assets/icon.png',
     userInterfaceStyle: 'automatic',
